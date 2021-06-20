dc20fda9bc8944be9ac45523aed9ece692f9f717
Kim-777 <60419368+Kim-777@users.noreply.github.com>
2021-06-20 13:18:54 +0900

    native: useState

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+  const [ name, setName ] = useState('yechan');
+  const [person, setPerson] = useState({name: 'mario', age: 10});
+
+  const clickHandler = () => {
+    setName('핸썸 예찬');
+    setPerson({name: '여우', age: 20});
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.boldText} >Hello, World!</Text>
+      </View>
+      <View>
+        <Text>제 이름은 {name}입니다!</Text>
+        <Text>제 친구 이름은 {person.name}이고 나이는 {person.age}입니다.</Text>
+        <View style={styles.buttonContainer}><Button title='change name' onPress={clickHandler}/></View>
+      </View>
     </View>
   );
 }
@@ -18,4 +33,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: 'pink',
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: 'bold'
+  },
+  buttonContainer: {
+    marginTop: 20
+  }
 });
